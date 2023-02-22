dfcb89c30079b49ded2eab444ea89ad5c84ec474
Ty Aponte <aaustinsk8@aol.com>
2023-02-22 13:17:33 -0800

    ziplists coded

--- linkedList/index.js
+++ linkedList/index.js
@@ -81,6 +81,20 @@ class LinkedList {
   }
 }
 
+const ziplists = (list1,list2) => {
+  let current1 = list1.head;
+  let current2 = list2.head;
+  let list3 = new LinkedList();
+  while(current1||current2){
+    list3.append(current1.value);
+    current1 = current1.next;
+  }
+  if(current2){
+    list3.append(current2.value);
+    current2 = current2.next;
+  }
+  return list3;
+}
 
 
 
