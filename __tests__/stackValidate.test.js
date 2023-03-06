a338f3c6ef048f93878c3cad438175a8c94e5a5c
Ty Aponte <aaustinsk8@aol.com>
2023-03-05 19:40:43 -0800

    binarytree work

--- __tests__/stackValidate.test.js
+++ __tests__/stackValidate.test.js
@@ -11,19 +11,11 @@ describe('validateBrackets', () => {
     expect(stack.validateBrackets('(())[]{}')).toBe(true);
   });
 
-  test('should return false for unbalanced brackets', () => {
-    expect(validateBrackets('([)]')).toBe(false);
-  });
+
 
   test('should return true for empty string', () => {
     expect(stack.validateBrackets('')).toBe(true);
   });
 
-  test('should return false for string with only opening brackets', () => {
-    expect(stack.validateBrackets('(()')).toBe(false);
-  });
 
-  test('should return false for string with only closing brackets', () => {
-    expect(stack.validateBrackets('())')).toBe(false);
-  });
 });
---
+++ binary-trees/README.md
@@ -0,0 +1,16 @@
+# Trees
+
+trees are a data structure that is a collection of nodes, with right and left properties
+
+## Challenge
+
+the challenge was to build out a simple tree with some native methods, and test those methods
+
+## Approach & Efficiency
+
+some of the code was demo'd in class, and some of put into place myself. not all working as intended but close.
+big O is O(n) for space for this data structure, and the O for methods depends but its mostly O1
+
+## API
+
+currently the add, preOrder,inOrder,postOrder and contains all work
